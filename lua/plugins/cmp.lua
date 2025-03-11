ed3c649700581a0656d0162123c3041dac2c9ee9
p4wnda <p4wnda@gmail.com>
2025-03-11 18:20:13 +0100

    update nvim

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,15 +1,20 @@
 return {
   "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-nvim-lsp" },
   config = function()
     local cmp = require("cmp")
+
     cmp.setup({
-      formatting = {
-        format = function(entry, vim_item)
-          -- Fixes the "format: " issue in completion
-          vim_item.abbr = vim_item.abbr:gsub("format: ", "")
-          return vim_item
-        end,
-      },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp", keyword_length = 1 }, -- 🔥 LSP Autocompletion
+        { name = "buffer" },
+        { name = "path" },
+      }),
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
     })
   end,
 }
